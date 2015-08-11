d93ef21f67c3c820209a2215f71dba83a939f5f7
Fedor Tatarskij <fedor.tatarskij@rubius.com>
2015-08-11 09:13:37 +0600

    +

--- towards/py.py
+++ towards/py.py
@@ -1,6 +1,6 @@
 
-import sys
-sys.path.append('/usr/local/lib/python2.7/dist-packages')
-print '\n'.join(sys.path)
+#import sys
+#sys.path.append('/usr/local/lib/python2.7/dist-packages')
+#print '\n'.join(sys.path)
 import numpy
 
